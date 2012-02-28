e441c35b054453bfbe66d0f75d412115a0a091e7
Kai Ito <kxi8435@rit.edu>
2012-02-28 13:44:13 -0800

    recommented

--- webbot/model/__init__.py
+++ webbot/model/__init__.py
@@ -19,9 +19,9 @@ DeclarativeBase = declarative_base()
 
 # There are two convenient ways for you to spare some typing.
 # You can have a query property on all your model classes by doing this:
-# DeclarativeBase.query = DBSession.query_property()
+DeclarativeBase.query = DBSession.query_property()
 # Or you can use a session-aware mapper as it was used in TurboGears 1:
-DeclarativeBase = declarative_base(mapper=DBSession.mapper)
+#DeclarativeBase = declarative_base(mapper=DBSession.mapper)
 
 # Global metadata.
 # The default metadata is the one from the declarative base.
