27dfcb35e62f212b7dae1c3c2eb90c87f9a0c0c0
Nathaniel Case <Qalthos@gmail.com>
2012-03-08 10:38:21 -0800

    Whitespace.

--- webbot/controllers/root.py
+++ webbot/controllers/root.py
@@ -65,12 +65,12 @@ class RootController(BaseController):
     @expose()
     def do_logout(self, name):
         query = model.Login.query.filter_by(name=name)
-        
+
         if query.count() == 0:
             # wtf...  when would this happen?
             log_message("'%s' (who DNE) tried to logout." % name)
             redirect('http://ritfloss.rtfd.org/')
-        
+
         user = query.first()
         log_message("'%s' logged out." % user.name)
         model.DBSession.delete(user)
@@ -79,7 +79,7 @@ class RootController(BaseController):
     @expose()
     def do_login(self, name, access_token):
         query = model.Login.query.filter_by(name=name)
-        
+
         if query.count() == 0:
             user = model.Login(name=name)
             model.DBSession.add(user)
