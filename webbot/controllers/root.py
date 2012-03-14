970d986a184ce5eca532a5d3486d0b88b6532fc2
Nathaniel Case <Qalthos@gmail.com>
2012-03-14 14:55:13 -0700

    this was my fault too

--- webbot/controllers/root.py
+++ webbot/controllers/root.py
@@ -185,7 +185,7 @@ class RootController(BaseController):
         user_id = kwargs['uid']
         for item in data['data']:
             uid = item['uid2']
-            if DBSession.query(model.Login).filter_by(userid == uid):
+            if DBSession.query(model.Login).filter_by(id=uid):
                 if not DBSession.query(model.Friend).filter_by(uid_left=user_id).filter_by(uid_right=uid):
                     friendship = model.Friend(uid_left=user_id, uid_right=uid)
                     DBSession.add(friendship)
